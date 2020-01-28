a27dd8633051cd2194e7d96e2ee6b98ce331e03e
JPlante9117 <jacrplante@gmail.com>
2020-01-28 13:59:08 -0500

    Done.

--- index.js
+++ index.js
@@ -12,5 +12,9 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(str => {
+    return str.split(' ').map(word => {
+      return word.replace(word[0], word[0].toUpperCase())
+    }).join(" ")
+  })
 }
