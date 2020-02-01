4ea1283bdb8dcab2589af072743d4a1f88bedea9
lmjcbs <hello@liamjacobs.dev>
2020-02-01 09:49:51 +0000

    Done.

--- index.js
+++ index.js
@@ -12,5 +12,9 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(function (string) {
+    return string.split(' ').map(function (word) {
+      return word.replace(word[0], word[0].toUpperCase())
+    }).join(' ')
+  })
 }
