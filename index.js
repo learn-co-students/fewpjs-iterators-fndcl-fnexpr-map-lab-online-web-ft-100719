25353c930bed973946fe6f8d6b362a107f307e5c
Emily <emily.a.harber@gmail.com>
2020-02-04 13:02:26 -0500

    Done.

--- index.js
+++ index.js
@@ -11,6 +11,16 @@ const tutorials = [
   'what is JSONP?'
 ];
 
+
 const titleCased = () => {
-  return tutorials
+
+  let capitalizedTutorials = tutorials.map( sentence => {
+    let splitSentence = sentence.split(" ")
+    let capitalSentence = splitSentence.map( word => {
+      return word.charAt(0).toUpperCase() + word.slice(1)
+    })
+    return capitalSentence.join(" ")
+  })
+
+  return capitalizedTutorials
 }
